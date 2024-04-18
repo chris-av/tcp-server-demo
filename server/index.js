7ee7cfbcfaef48e56b2b34a0edb37218f8b415e9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-04-18 01:13:30 -0700

    replace array with map

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
 const net = require('net');
 const port = process.env.PORT || 7070;
 
-let sockets = [];
+const sockets = new Map();
 
 const server = net.createServer(function(socket) {
   const { remoteAddress: addr, remotePort: port } = socket;
   const client = `${addr} (${port})`;
   let date = new Date().toLocaleString();
   console.log(`${date} CONNECTION : ${client})`);
-  sockets.push(socket);
+  sockets.set(client, socket);
   socket.on('data', function(data) {
     let date = new Date().toLocaleString();
     console.log(`${date} DATA : ${addr} (${port})`);
-    for (let i = 0; i < sockets.length; i++) {
-      if (sockets[i] === socket) continue;
-      sockets[i].write(`${client}) : ${data}`);
+    for (const s of sockets.values()) {
+      if (s === socket) { continue; }
+      s.write(`${client} : ${data.toString()}`);
     }
   });
   socket.on('close', function() {
-    let index = sockets.indexOf(socket);
-    let date = new Date().toLocaleString();
-    sockets.splice(index, 1);
+    sockets.delete(client);
     console.log(`${date} CLOSED CONNECTION : ${client})`);
   });
 });
