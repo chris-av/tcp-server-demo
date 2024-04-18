18b91b265cd52fff0c3fb8958e3a90a2c8d72c1c
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-04-17 20:49:55 -0700

    refactor log string

--- server/index.js
+++ server/index.js
@@ -4,23 +4,24 @@ const port = process.env.PORT || 7070;
 let sockets = [];
 
 const server = net.createServer(function(socket) {
-  const { remoteAddress, remotePort } = socket;
+  const { remoteAddress: addr, remotePort: port } = socket;
+  const client = `${addr} (${port})`;
   let date = new Date().toLocaleString();
-  console.log(`CONNECTION : ${remoteAddress} (${remotePort}) ${date}`);
+  console.log(`${date} CONNECTION : ${client})`);
   sockets.push(socket);
   socket.on('data', function(data) {
     let date = new Date().toLocaleString();
-    console.log(`DATA : ${remoteAddress} (${remotePort}) ${date}`);
+    console.log(`${date} DATA : ${addr} (${port})`);
     for (let i = 0; i < sockets.length; i++) {
       if (sockets[i] === socket) continue;
-      sockets[i].write(`${remoteAddress} (${remotePort}) : ${data}`);
+      sockets[i].write(`${client}) : ${data}`);
     }
   });
   socket.on('close', function() {
     let index = sockets.indexOf(socket);
     let date = new Date().toLocaleString();
     sockets.splice(index, 1);
-    console.log(`CLOSED CONNECTION : ${remoteAddress} (${remotePort}) ${date}`);
+    console.log(`${date} CLOSED CONNECTION : ${client})`);
   });
 });
 
